5d3db0925c9c01beba6d146956f5ec7340a26a00
Dina Singatulina <dina.sing1212@gmail.com>
2020-08-18 18:26:48 +0300

    feat: add movie time's and seats type's reducers

--- src/app/common/reducers/rootReducer.js
+++ src/app/common/reducers/rootReducer.js
@@ -2,11 +2,15 @@ import { combineReducers } from 'redux';
 import authReducer from '../../auth/reducers/authReducer';
 import errorReducer from './errorReducer';
 import movieReducer from '../../movie/reducers/movieReducer';
+import movieTimeReducer from '../../movie times/reducers/movieTimeReducer';
+import seatTypeReducer from '../../seat type/reducers/seatTypeReducer';
 
 const rootReducer = combineReducers({
   auth: authReducer,
   error: errorReducer,
   movie: movieReducer,
+  movieTime: movieTimeReducer,
+  seatType: seatTypeReducer,
 });
 
 export default rootReducer;
