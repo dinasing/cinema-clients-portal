4717608b28fb70e1c72e4c9dfb8db1fdef0886a8
Dina Singatulina <dina.sing1212@gmail.com>
2020-08-19 21:03:50 +0300

    feat: update hall's schema o show booked seats

--- src/app/cinema hall/components/HallSchema.js
+++ src/app/cinema hall/components/HallSchema.js
@@ -1,36 +1,53 @@
 import React, { Component } from 'react';
-import { Badge, UncontrolledTooltip, Container } from 'reactstrap';
+import { Badge, UncontrolledTooltip, Container, Button } from 'reactstrap';
 
 export default class HallSchema extends Component {
   render() {
-    const { schema, hallTitle, seatTypes } = this.props;
-    return <Seats schema={schema} hallTitle={hallTitle} seatTypes={seatTypes} />;
+    const { schema, hallTitle, seatTypes, bookedSeats } = this.props;
+    return (
+      <Seats
+        schema={schema}
+        hallTitle={hallTitle}
+        seatTypes={seatTypes}
+        bookedSeats={bookedSeats}
+      />
+    );
   }
 }
 
 const Seats = props => {
-  const { schema, hallTitle, seatTypes } = props;
+  const { schema, hallTitle, seatTypes, bookedSeats } = props;
 
   const seats = schema.map((row, rowIndex) => {
-    const rowSeats = new Array(Number(row.numberOfSeats)).fill().map((seat, seatIndex) => (
-      <>
-        <Badge color="primary" id={`hallTitle-${hallTitle}_row${rowIndex + 1}seat${seatIndex + 1}`}>
-          {+seatTypes.find(seatType => seatType.id === +row.seatsType).numberOfPeople === 1 ? (
-            seatIndex + 1
-          ) : (
-            <Container>{` ${seatIndex + 1}`}</Container>
-          )}
-        </Badge>{' '}
-        <UncontrolledTooltip
-          placement="right"
-          target={`hallTitle-${hallTitle}_row${rowIndex + 1}seat${seatIndex + 1}`}
-        >
-          <p>{`row: ${rowIndex + 1}`}</p>
-          <p>{`seat: ${seatIndex + 1}`}</p>
-          <p>{`type: ${seatTypes.find(seatType => seatType.id === +row.seatsType).title}`}</p>
-        </UncontrolledTooltip>
-      </>
-    ));
+    const rowSeats = new Array(Number(row.numberOfSeats)).fill().map((seat, seatIndex) => {
+      return (
+        <>
+          <Button
+            color="primary"
+            id={`hallTitle-${hallTitle}_row${rowIndex + 1}seat${seatIndex + 1}`}
+            disabled={bookedSeats.some(
+              bookedSeat => bookedSeat.row === rowIndex && bookedSeat.seat === seatIndex
+            )}
+          >
+            <Badge color="primary">
+              {+seatTypes.find(seatType => seatType.id === +row.seatsType).numberOfPeople === 1 ? (
+                seatIndex + 1
+              ) : (
+                <Container>{`${seatIndex + 1}`}</Container>
+              )}
+            </Badge>
+          </Button>{' '}
+          <UncontrolledTooltip
+            placement="right"
+            target={`hallTitle-${hallTitle}_row${rowIndex + 1}seat${seatIndex + 1}`}
+          >
+            <p>{`row: ${rowIndex + 1}`}</p>
+            <p>{`seat: ${seatIndex + 1}`}</p>
+            <p>{`type: ${seatTypes.find(seatType => seatType.id === +row.seatsType).title}`}</p>
+          </UncontrolledTooltip>
+        </>
+      );
+    });
 
     const newRow = (
       <h6 className="text-center">
