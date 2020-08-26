2f2026ec3acda0693443cec97cf59470d0d2e3f4
Dina Singatulina <dina.sing1212@gmail.com>
2020-08-27 00:34:46 +0300

    fix: delete cons init

--- src/index.js
+++ src/index.js
@@ -4,6 +4,5 @@ import App from './app/common/components/App';
 import { hot } from 'react-hot-loader';
 
 const AppWithHot = hot(module)(App);
-const socket = io("http://localhost:3000");
 
 ReactDOM.render(<AppWithHot />, document.getElementById('app'));
