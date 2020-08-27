6b4770dc9bd08b8b2c4567e2597cb9b9d9b234f1
Dina Singatulina <dina.sing1212@gmail.com>
2020-08-27 13:50:16 +0300

    feat: add route for adding snacks to ticket

--- src/app/common/components/Routes.js
+++ src/app/common/components/Routes.js
@@ -8,10 +8,13 @@ import SignUp from '../../auth/components/SignUp';
 import Movies from '../../movie/components/MovieList';
 import Movie from '../../movie/components/Movie';
 import BookingContainer from '../../movie times/components/BookingContainer';
+import AdditionalGoodsListContainer from '../../movie times/components/AdditionalGoodsListContainer';
 
 class Routes extends Component {
   render() {
     const { isAuthenticated } = this.props.auth;
+    const { seatsPreparedForBooking } = this.props.movieTime;
+
     return (
       <Switch>
         <Route
@@ -27,6 +30,19 @@ class Routes extends Component {
 
         <Route exact path="/session/:movie_time_id" component={BookingContainer} store={store} />
 
+        <Route
+          exact
+          path="/additional-goods"
+          render={() =>
+            isAuthenticated && seatsPreparedForBooking.length ? (
+              <AdditionalGoodsListContainer />
+            ) : (
+              <Redirect to="/movies" />
+            )
+          }
+          store={store}
+        />
+
         <Route path="*" component={() => '404 NOT FOUND'} />
       </Switch>
     );
@@ -35,6 +51,7 @@ class Routes extends Component {
 
 const mapStateToProps = state => ({
   auth: state.rootReducer.auth,
+  movieTime: state.rootReducer.movieTime,
 });
 
 export default connect(mapStateToProps, null)(Routes);
