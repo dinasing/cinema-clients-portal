6c825c88bfa719827591e4e25c468dc773f977a9
Dina Singatulina <dina.sing1212@gmail.com>
2020-08-31 00:56:47 +0300

    feat: add photo to single cinema page

--- src/app/cinema/components/Cinema.js
+++ src/app/cinema/components/Cinema.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { Media, Container } from 'reactstrap';
 import { getCinemaById, getMovieTimes } from '../actions/cinemaAction';
 import MovieTimesList from '../../movie times/components/MovieTimesList';
 
@@ -16,11 +17,24 @@ class MovieTheater extends Component {
 
     return (
       <>
-        <h2>{cinema.title}</h2>
-        <small>{cinema.description}</small>
-        <p>
-          {cinema.city}, {cinema.address}
-        </p>
+        <Media>
+          <img
+            width="250px"
+            src={
+              cinema.photo
+                ? cinema.photo
+                : 'https://i.pinimg.com/originals/8a/eb/d8/8aebd875fbddd22bf3971c3a7159bdc7.png'
+            }
+          />{' '}
+          <Container>
+            <h2>{cinema.title}</h2>
+            <small>{cinema.description}</small>
+            <p>
+              {cinema.city}, {cinema.address}
+            </p>
+          </Container>
+        </Media>
+        <br />
         <h5>Movie sessions</h5>
         {movieTimes.length ? (
           <MovieTimesList movieTimes={movieTimes} />
