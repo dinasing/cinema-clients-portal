2461df339f018931e592d8f7058b188ac76d7a4b
Dina Singatulina <dina.sing1212@gmail.com>
2020-09-02 01:00:06 +0300

    feat: change movieDB API key init

--- src/app/movie/actions/movieAction.js
+++ src/app/movie/actions/movieAction.js
@@ -9,10 +9,9 @@ import {
   GET_GENRES_FAIL,
   GET_GENRES,
 } from '../../common/actions/types';
-import { returnErrors, clearErrors } from '../../common/actions/errorAction';
-import { tokenConfig } from '../../auth/actions/authAction';
+import { returnErrors } from '../../common/actions/errorAction';
 
-const apiKey = process.env.API_KEY;
+const apiKey = process.env.MOVIE_DB_API_KEY;
 
 export const getMovies = () => dispatch => {
   dispatch(setMoviesLoading());
