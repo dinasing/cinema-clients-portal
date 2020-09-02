f01f43dca52b78aad773807cd5348f09bee9173c
Dina Singatulina <dina.sing1212@gmail.com>
2020-09-02 14:50:10 +0300

    feat: add userReduser to rootReducer

--- src/app/common/reducers/rootReducer.js
+++ src/app/common/reducers/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import authReducer from '../../auth/reducers/authReducer';
 import errorReducer from './errorReducer';
 import movieReducer from '../../movie/reducers/movieReducer';
+import userReducer from '../../user/reducers/userReducer';
 
 const rootReducer = combineReducers({
   auth: authReducer,
   error: errorReducer,
   movie: movieReducer,
+  user: userReducer,
 });
 
 export default rootReducer;
