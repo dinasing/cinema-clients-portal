aa8c432928794aba0514c09d058565d203b5323f
Dina Singatulina <dina.sing1212@gmail.com>
2020-09-09 15:50:56 +0300

    feat: remove seatsBookedByUser prop

--- src/app/movie times/components/HallSchema.js
+++ src/app/movie times/components/HallSchema.js
@@ -3,14 +3,7 @@ import { Badge, UncontrolledTooltip, Container, Button } from 'reactstrap';
 
 export default class HallSchema extends Component {
   render() {
-    const {
-      schema,
-      hallTitle,
-      seatTypes,
-      bookedSeats,
-      selectedSeats,
-      seatsBookedByUser,
-    } = this.props;
+    const { schema, hallTitle, seatTypes, bookedSeats, selectedSeats } = this.props;
 
     return (
       <>
@@ -22,7 +15,6 @@ export default class HallSchema extends Component {
           bookedSeats={bookedSeats}
           selectedSeats={selectedSeats}
           handleSelectSeat={this.props.handleSelectSeat}
-          seatsBookedByUser={seatsBookedByUser}
         />
       </>
     );
@@ -30,7 +22,7 @@ export default class HallSchema extends Component {
 }
 
 const Seats = props => {
-  const { schema, hallTitle, seatTypes, bookedSeats, selectedSeats, seatsBookedByUser } = props;
+  const { schema, hallTitle, seatTypes, bookedSeats, selectedSeats } = props;
 
   const seats = schema.map((row, rowIndex) => {
     const rowSeats = new Array(Number(row.numberOfSeats)).fill().map((seat, seatIndex) => {
@@ -39,21 +31,16 @@ const Seats = props => {
           <Button
             onClick={props.handleSelectSeat(rowIndex, seatIndex, +row.seatsType)}
             color={
-              selectedSeats.some(
+              selectedSeats.find(
                 selectedSeat => selectedSeat.row == rowIndex && selectedSeat.seat == seatIndex
               )
                 ? 'warning'
-                : seatsBookedByUser.length &&
-                  seatsBookedByUser.some(
-                    selectedSeat => selectedSeat.row == rowIndex && selectedSeat.seat == seatIndex
-                  )
-                ? 'success'
                 : 'primary'
             }
             id={`hallTitle-${hallTitle}_row${rowIndex + 1}seat${seatIndex + 1}`}
             disabled={
               bookedSeats.length
-                ? bookedSeats.some(
+                ? bookedSeats.find(
                     bookedSeat => bookedSeat.row === rowIndex && bookedSeat.seat === seatIndex
                   )
                 : false
@@ -61,15 +48,10 @@ const Seats = props => {
           >
             <Badge
               color={
-                selectedSeats.some(
+                selectedSeats.find(
                   selectedSeat => selectedSeat.row == rowIndex && selectedSeat.seat == seatIndex
                 )
                   ? 'warning'
-                  : seatsBookedByUser.length &&
-                    seatsBookedByUser.some(
-                      selectedSeat => selectedSeat.row == rowIndex && selectedSeat.seat == seatIndex
-                    )
-                  ? 'success'
                   : 'primary'
               }
             >
